8d45c0eb6ae7ec0d8de8519e7dcbcb34f4d71fb2
OwenLinden <ow.linden@hotmail.com>
2024-02-20 10:32:27 -0400

    Update ColorGrid.rkt

--- ColorGrid.rkt
+++ ColorGrid.rkt
@@ -1,30 +1,60 @@
 #lang racket/gui
+
+
 (define frame(new frame%
-                  [label "Test GUI"]
-                  [width 400]
-                  [height 400]))
+                  [label "Color grid"]
+                  [width 350]
+                  [height 445]))
+
+(define(board-setup dc)
+  (send dc clear))
+
+(define board(new canvas%
+                   [parent frame]
+                   [paint-callback
+                    (lambda (canvas dc)
+                      (board-setup dc))]))
+
+
+
 (define(new-grid)
-  #(#(111 222 33 44)
+  #(#(222 43 133 64)
     #(44 55 66 77)
-    #(88 99 88 77)
-    #(66 55 44 33)))
+    #(118 99 88 77)
+    #(6 78 244 33)
+    #(45 123 234 90)))
 
 
-(define(canvas-paint dc)
+
+(define(board-paint dc)
   (send dc clear)
-
+  
   (let*([grid(new-grid)]
         [grid-size(vector-length grid)]
         [cell-size(/ 400 grid-size)])
     (for*([row grid-size]
           [col grid-size])
-      (let([x (* cell-size row)]
+      (let*([x (* cell-size row)]
            [y (* cell-size col)]
-           [fill-col (make-color 2 2 (vector-ref(vector-ref grid col)row))])
+           [block (vector-ref(vector-ref grid col)row)]
+           [fill-col (make-color block 1 block )])
+        
         (send dc set-pen fill-col 1 'solid)
         (send dc set-brush fill-col 'solid)
-        (send dc draw-rectangle (add1 x) (add1 y) (sub1 cell-size) (sub1 cell-size))
-        )
-      )
-    )
-  )
+        (send dc draw-rectangle (add1 x) (add1 y) (sub1 cell-size) (sub1 cell-size))))))
+    
+
+(define timer (new timer%
+                   [notify-callback (λ () (timer-sent))]
+                   [interval #f]
+                   [just-once? #t]))
+
+(define (timer-sent)
+  (board-paint (send board get-dc)))
+
+
+(define (run)
+  (send frame show #t)
+  (send timer start 1000))
+
+  
